29feb06818dc5e10294cb39843e4ce4d31cc2837
lavanyasaravanans <53213595+lavanyasaravanans@users.noreply.github.com>
2019-08-09 00:53:14 -0700

    Add files via upload

---
+++ pla11.py
@@ -0,0 +1,15 @@
+ho=str(input(""))
+if ho=="Monday":
+    print("no")
+elif ho=="Tuesday":
+    print("no")
+elif ho=="Wednesday":
+    print("no")
+elif ho=="Thursday":
+    print("no")
+elif ho=="Friday":
+    print("no")
+elif ho=="Saturday":
+    print("yes")
+elif ho=="Sunday":
+    print("yes")
