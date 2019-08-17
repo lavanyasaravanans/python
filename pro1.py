bea02d8daf8ee95181cc8f424317adb1422dff03
lavanyasaravanans <53213595+lavanyasaravanans@users.noreply.github.com>
2019-08-16 23:43:36 -0700

    Add files via upload

--- pro1.py
+++ pro1.py
@@ -1,13 +1,28 @@
-i=int(input())
-l=(input().split())[0:i]
-o=i//2
-if i>3:
-  if(o%2==0):
-    print("yes")
-  else:
-    print("no")
-else:
-  if(i==3):
-    print("yes")
-  else:
-    print("no")
+def minl(s,x):
+    min=len(s[0])
+    for i in range(1,x):
+        if len(s[i])<min:
+           min=len(s[i])
+    return(min)
+def commonprefix(s,x):
+    minlen=minl(s,x)
+    result=""
+    for i in range(minlen):
+        crn=s[0][i]
+        for j in range(1,x):
+            if(s[j][i]!=crn):
+                return result
+        result=result+crn
+    return(result)
+if __name__=="__main__":
+    no=int(input())
+    s=[]
+    for n in range(0,no):
+        ss=str(input())
+        s.append(ss)
+    x=len(s)
+    final=commonprefix(s,x)
+    if(len(final)):
+        print(final)
+    else:
+        print("")
